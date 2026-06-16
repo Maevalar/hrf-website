b8054b0f482c3572494f97e0c54ea5696fd00844
Mithrandir <diguerra523@gmail.com>
2026-06-16 14:37:31 +0300

    fear(SCRUM-9): finished MainButton and Logo components

---
+++ src/components/atoms/logo/Logo.tsx
@@ -0,0 +1,10 @@
+
+type LogoProps = {
+    className?: string;
+}
+
+export const Logo: React.FC<LogoProps> = ({ className = ''}) => {
+    return (
+        <img src="logo.svg" alt="" className = { className }/>
+    )
+}
